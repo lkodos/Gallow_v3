4df927f55cb01c367db8ece49cd8edb0b8a570d9
lkodos <tshagimardanov@gmail.com>
2025-02-03 21:54:50 +0300

    finish method startGame

--- src/ru/lkodos/Game.java
+++ src/ru/lkodos/Game.java
@@ -12,20 +12,21 @@ public class Game {
         while (true) {
             System.out.println("1 - Новая игра");
             System.out.println("2 - Выход");
-            int choice = System.in.read();
-            if (choice == 49) {
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) {
                 newGame();
-            } else if (choice == 50) {
+            } else if (choice.equals("2")) {
                 System.out.println("Конец игры");
                 System.exit(0);
             } else {
                 System.out.println("Некорректный ввод");
-                System.in.skip(System.in.available());
             }
         }
     }
 
     private static void newGame() {
         System.out.println("start new game");
+        System.out.println();
     }
 }
