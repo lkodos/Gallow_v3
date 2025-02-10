176872f60bb818375be5c8ae9b1ef42fa635e9c0
lkodos <tshagimardanov@gmail.com>
2025-02-10 15:24:15 +0300

    first finished version

--- src/ru/lkodos/Game.java
+++ src/ru/lkodos/Game.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 public class Game {
 
-    public static void startGame() {
+    static Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    public static void startGame() {
 
         while (true) {
             System.out.println("1 - Новая игра");
@@ -19,6 +19,7 @@ public class Game {
                 newGame();
             } else if (choice.equals("2")) {
                 System.out.println("Конец игры");
+                scanner.close();
                 System.exit(0);
             } else {
                 System.out.println("Некорректный ввод");
@@ -29,34 +30,72 @@ public class Game {
     private static void newGame() {
 
         List<String> currentSecretWord = new ArrayList<>();
+        List<String> usedLetters = new ArrayList<>();
         int levelOfGallow = 0;
         boolean isWin = false;
+        String letter;
 
         List<String> secretWord = WordService.getListFromString();
         currentSecretWord = secretWordToStars(secretWord);
-        showCurrentWord(secretWord);
-        showCurrentWord(currentSecretWord);
-        System.out.println();
 
-        while (levelOfGallow < Gallow.gallows.size()) {
+        while (levelOfGallow < Gallow.gallows.size() - 1) {
             if (secretWord.equals(currentSecretWord)) {
                 isWin = true;
                 break;
+            }
+
+            System.out.println("(" + secretWord + ")");
+            System.out.print("Загадано слово: ");
+            showCurrentWord(currentSecretWord);
+            System.out.println();
+            System.out.println(Gallow.getGallow(levelOfGallow));
+            System.out.println("Использованные буквы " + usedLetters);
+            System.out.println("Введите букву: ");
+            System.out.println();
+
+            while (true) {
+                letter = scanner.nextLine();
+                if (alreadyBeen(usedLetters, letter)) {
+                    System.out.println("Такая буква уже была, повторите ввод");
+                } else {
+                    usedLetters.add(letter);
+                    break;
+                }
+            }
+
+            if (openLetterInSecretWord(letter, secretWord, currentSecretWord)) {
+                System.out.println("Есть такая буква в этом слове");
+                System.out.println(currentSecretWord);
+                System.out.println();
+            }
+            else {
+                System.out.println("Такой буквы нет");
+                levelOfGallow++;
             }
         }
 
         if (isWin) {
             System.out.println("ПБЕДА!!!");
+            System.out.println();
         } else {
             System.out.println("ВАМ КОНЕЦ");
+            System.out.println(Gallow.getGallow(levelOfGallow));
         }
+    }
 
+    private static boolean openLetterInSecretWord(String letter, List<String> secretWord, List<String> currentSecretWord) {
+        boolean isPresent = false;
+        for (int i = 0; i < secretWord.size(); i++) {
+            if (letter.equals(secretWord.get(i))) {
+                currentSecretWord.set(i, letter);
+                isPresent = true;
+            }
+        }
+        return isPresent;
+    }
 
-
-
-
-
-
+    private static boolean alreadyBeen(List<String> usedLetters, String letter) {
+        return usedLetters.contains(letter);
     }
 
     private static List<String> secretWordToStars(List<String> secretWord) {
